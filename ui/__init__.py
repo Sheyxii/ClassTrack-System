721043b0f91d20181b0fa34758bd2d4cb76fe6f0
MarcianoSalazar <salazarjrmarciano@gmail.com>
2025-11-21 22:43:01 +0800

    Add UI module imports initialization

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,3 +2,9 @@ from .main_window import MainWindow
 from .login_window import LoginWindow
 from .signup_dialog import SignupDialog
 from .forgot_password_dialog import ForgotPasswordDialog
+from .dashboard_page import DashboardPage
+from .my_classes_page import MyClassesPage
+from .attendance_page import AttendancePage
+from .schedule_page import SchedulePage
+from .resources_page import ResourcesPage
+from .settings_page import SettingsPage
